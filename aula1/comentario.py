219d1f0033dc398a59387b090601961c17bcd94c
majunior2 <150537270+majunior2@users.noreply.github.com>
2024-09-11 10:39:36 -0300

    Add files via upload

---
+++ aula1/comentario.py
@@ -0,0 +1,12 @@
+#comentario de linha
+
+"""
+este é o comentario denominado 
+DocString
+Comentario de bloco
+"""
+'''
+use para escrever suas notas 
+'''
+#<!--Comentando em tela HTML-->
+print("")
---
+++ aula1/variavel.py
@@ -0,0 +1,10 @@
+# Entrada
+nome = "Vanessa" #texto, String
+idade = 32  # inteiro
+altura = 1.70 # float
+
+#Saída
+print("Nome:", nome)
+print("Idade: ",idade)
+print("Altura :", altura)
+print("Nome",nome, 'Idade', idade, 'altura',altura)
